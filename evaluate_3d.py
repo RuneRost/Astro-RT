bbb9355b2581f1775e81458d56343710685a00d6
RuneRost <ruro20157@gmail.com>
2025-08-29 03:25:08 +0200

    final changes

--- evaluate_3d.py
+++ evaluate_3d.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 os.environ["OMP_NUM_THREADS"] = "8"
 os.environ["MKL_NUM_THREADS"] = "8"
 os.environ["OPENBLAS_NUM_THREADS"] = "8"
